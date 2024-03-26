bc37b188550905f495ef0bbd664e9eefd2f4ca55
Fikunmi17 <127734958+Fikunmi17@users.noreply.github.com>
2024-03-26 22:47:17 +0100

    Add files via upload

---
+++ components/navbar.component.jsx
@@ -0,0 +1,16 @@
+
+import invaders from "../images/partners/invaders.png"
+
+const Navbar = () =>{
+    return(
+        // NAVBAR
+        <nav className="navbar">
+            {/* LOGO */} 
+           <Link>
+            
+           </Link>
+        </nav>
+    )
+}
+
+export default Navbar;
